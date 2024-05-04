5a0c847f322d2ba56f42c02e1eb7570dd8adaf4a
baboshi <abdelhameid@abdelhameid>
2024-05-05 01:08:51 +0200

    Basic Search Functionality Added

---
+++ app/api/products/route.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (req: NextRequest) => {
+  try {
+    const res = await fetch("https://fakestoreapi.com/products").then((res) =>
+      res.json()
+    );
+    return NextResponse.json(res, { status: 200 });
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json({}, { status: 404, statusText: "Error fetching" });
+  }
+};
--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,10 +1,27 @@
 import React, { SyntheticEvent, useState } from "react";
+import { ProductType } from "../lib/interfaces/ProductType";
 
 const SearchBar = () => {
   const [query, setQuery] = useState("");
+  const [response, setResponse] = useState<any>([]);
+  const [isError, setIsError] = useState(false);
+  const handleSubmit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    const response = await fetch("/api/products");
+    if (response.status !== 200) {
+      setIsError(true);
+    } else {
+      setIsError(false);
+      const data = await response.json();
+      const filteredData = data.filter((entry: ProductType) =>
+        entry.title.includes(query)
+      );
+      setResponse(data);
+    }
+  };
   return (
     <div className="relative group ">
-      <form onSubmit={(e: SyntheticEvent) => e.preventDefault()}>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Search... "
@@ -12,8 +29,14 @@ const SearchBar = () => {
           onChange={(e) => setQuery(e.target.value)}
         />
       </form>
-      <div className="h-10 w-full absolute hidden  flex-col gap-1 top-8  group-hover:flex ">
-        <div className="bg-pink-300"> {query}</div>
+      <div className="h-10 w-full absolute hidden  flex-col gap-1 top-8  group-hover:flex bg-gray-600">
+        {isError ? (
+          <>Error</>
+        ) : (
+          response.map((r: ProductType) => (
+            <div className="bg-pink-400"> {r.title}</div>
+          ))
+        )}
       </div>
     </div>
   );
