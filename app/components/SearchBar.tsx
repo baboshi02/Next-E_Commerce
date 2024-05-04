4ca0dab123d12f0b7c5beda6fad52bd7ff537d61
baboshi <abdelhameid@abdelhameid>
2024-05-05 01:36:15 +0200

    Added use Effect to save changes

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,39 +1,49 @@
-import React, { SyntheticEvent, useState } from "react";
+import React, { SyntheticEvent, useEffect, useState } from "react";
 import { ProductType } from "../lib/interfaces/ProductType";
 
 const SearchBar = () => {
   const [query, setQuery] = useState("");
-  const [response, setResponse] = useState<any>([]);
+  const [filteredData, setFilteredData] = useState<Array<any>>([]);
+  const [response, setResponse] = useState<Array<ProductType | any>>([]);
   const [isError, setIsError] = useState(false);
-  const handleSubmit = async (e: SyntheticEvent) => {
-    e.preventDefault();
+  const handleSubmit = async () => {
     const response = await fetch("/api/products");
     if (response.status !== 200) {
       setIsError(true);
     } else {
       setIsError(false);
       const data = await response.json();
-      const filteredData = data.filter((entry: ProductType) =>
-        entry.title.includes(query)
-      );
       setResponse(data);
     }
   };
+  useEffect(() => {
+    handleSubmit();
+  }, []);
+  const handleChange = () => {
+    const _filteredData = response.filter((r: ProductType) =>
+      r.title.includes(query)
+    );
+    setFilteredData(_filteredData);
+  };
+  console.log(filteredData);
   return (
     <div className="relative group ">
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={(e) => e.preventDefault()}>
         <input
           type="text"
           placeholder="Search... "
           value={query}
-          onChange={(e) => setQuery(e.target.value)}
+          onChange={(e) => {
+            setQuery(e.target.value);
+            handleChange();
+          }}
         />
       </form>
       <div className="h-10 w-full absolute hidden  flex-col gap-1 top-8  group-hover:flex bg-gray-600">
         {isError ? (
           <>Error</>
         ) : (
-          response.map((r: ProductType) => (
+          filteredData?.map((r: ProductType) => (
             <div className="bg-pink-400"> {r.title}</div>
           ))
         )}
